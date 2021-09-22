f63e2edfb267caec2be27cfdef7d5bf68fcab0e5
jalvarez5625 <66888292+jalvarez5625@users.noreply.github.com>
2021-09-22 09:58:40 -0400

    Create RegisTeleOp.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RegisTeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RegisTeleOp.kt
@@ -9,15 +9,36 @@ class RegisTeleOp : RegisLinearOp() {
         while (opModeIsActive()) {
             val swerve = gamepad1.left_stick_x
             val thrust = gamepad1.left_stick_y.toDouble()
+            val spin = gamepad1.right_stick_x
+            val duck = gamepad1.left_trigger
+            val intake = gamepad1.right_trigger
             rightFrontMotor.power = thrust
             leftFrontMotor.power = thrust
             leftBackMotor.power = thrust
             rightBackMotor.power = thrust
+            duckMotor.power = duck
+            intakeMotor.power = intake
             if (swerve > 0) {
                 leftBackMotor.power *= 1 - swerve
+
             } else if (swerve < 0) {
                 rightBackMotor.power *= 1 + swerve
+
             }
+
+            if (spin > 0) {
+                leftBackMotor.power *= 1 - spin
+                leftFrontMotor.power *= 1 - spin
+                rightBackMotor.power *= -1 * ( 1- spin)
+                rightFrontMotor.power *= -1 * ( 1- spin)
+            } else if (spin < 0) {
+                rightBackMotor.power *= 1 + swerve
+                rightFrontMotor.power *= 1 + swerve
+                rightBackMotor.power *= -1 * (1 + swerve)
+                rightFrontMotor.power *= -1 * (1 + swerve)
+
+            }
+
             telemetry.addData("Status", "Running")
             telemetry.update()
         }
